c5266ec7babac7a69b8b96da44b593866c39b837
nov05 <215590+Nov05@users.noreply.github.com>
2025-02-28 22:35:42 -0600

    .

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -8,11 +8,17 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
+    std::string node_name = ros::this_node::getName();
+
     // TODO: Request a service and pass the velocities to it to drive the robot
     // Request to drive my robot forward
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
+
+    // Make the service call
+    if (!client.call(srv))
+        ROS_ERROR("%s: Failed to call service drive_to_target", node_name.c_str());
 }
 
 // This callback function continuously executes and reads the image data
@@ -39,7 +45,7 @@ void process_image_callback(const sensor_msgs::Image img)
         int blue = img.data[i + 2];
 
         // Next check if you found the white color ball
-        if (red == 1 && green == 1 && blue == 1)
+        if (red == 255 && green == 255 && blue == 0)
             count += 1;
     }
     ROS_INFO("yellow pixels detected: %d", count);
